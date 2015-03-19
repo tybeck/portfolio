492fa75e1cf2e4bb89c83bb03b0189d5042eaaef
Tyler Beck <y2xtreme600@yahoo.com>
2015-03-19 02:44:47 -0400

    Server Test

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-// initial commit
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(80);
+console.log('Server running');
